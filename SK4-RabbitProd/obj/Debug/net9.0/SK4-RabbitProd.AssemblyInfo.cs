8f7ac1425128095e57322329eaad1b15461c7415
Diego Montoliu <diego@montolius.com>
2025-12-16 08:36:45 +0100

    Clean Solution

--- SK4-RabbitProd/obj/Debug/net9.0/SK4-RabbitProd.AssemblyInfo.cs
+++ SK4-RabbitProd/obj/Debug/net9.0/SK4-RabbitProd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SK4-RabbitProd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d5db11dea71e5d263cbda02baa17b0675fc796")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2757dfdba48961a13c8de96361f412fff9ac8717")]
 [assembly: System.Reflection.AssemblyProductAttribute("SK4-RabbitProd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SK4-RabbitProd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SK5/obj/Debug/net8.0/SK5.AssemblyInfo.cs
+++ SK5/obj/Debug/net8.0/SK5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SK5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d5db11dea71e5d263cbda02baa17b0675fc796")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2757dfdba48961a13c8de96361f412fff9ac8717")]
 [assembly: System.Reflection.AssemblyProductAttribute("SK5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SK5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
